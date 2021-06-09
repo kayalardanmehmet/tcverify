85927410d596a246ba8d03813a8bb8de105ce94e
Mehmet Kaya <mehmet1994@gmail.com>
2021-06-09 19:15:11 +0300

    foreign id validation

--- tcverify.go
+++ tcverify.go
@@ -16,6 +16,10 @@ type Response struct {
 	TCKimlikNoDogrulaResult string `xml:"Body>TCKimlikNoDogrulaResponse>TCKimlikNoDogrulaResult"`
 }
 
+type ResponseForeign struct {
+	TCKimlikNoDogrulaResult string `xml:"Body>YabanciKimlikNoDogrulaResponse>YabanciKimlikNoDogrulaResult"`
+}
+
 func Validate(ID string) (bool, error) {
 	if !validatetc.Validate(ID) {
 		err := errors.New(ID + " tc numarası algoritmik olarak doğrulanamadı.")
@@ -53,6 +57,52 @@ func Check(ID string, name string, surname string, birthYear string) (bool, erro
 	defer resp.Body.Close()
 
 	response := Response{}
+	if err := xml.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return false, err
+	}
+
+	status, err := strconv.ParseBool(response.TCKimlikNoDogrulaResult)
+	if err != nil {
+		return false, err
+	}
+
+	if !status {
+		error := errors.New("Bu bilgileri ait vatandaşlık doğrulanamadı.")
+		return status, error
+	}
+	return status, nil
+}
+
+func CheckForeign(ID string, name string, surname string, birthDay string, birthMonth string, birthYear string) (bool, error) {
+	rawXml := []byte(`<?xml version="1.0" encoding="utf-8"?>
+		<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<YabanciKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
+					<KimlikNo>` + ID + `</KimlikNo>
+					<Ad>` + strings.ToUpperSpecial(unicode.TurkishCase, name) + `</Ad>
+					<Soyad>` + strings.ToUpperSpecial(unicode.TurkishCase, surname) + `</Soyad>
+					<DogumGun>` + birthYear + `</DogumGun>
+					<DogumAy>` + birthYear + `</DogumAy>
+					<DogumYil>` + birthYear + `</DogumYil>
+				</YabanciKimlikNoDogrula>
+			</soap:Body>
+		</soap:Envelope>`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://tckimlik.nvi.gov.tr/Service/KPSPublicYabanciDogrula.asmx", bytes.NewBuffer(rawXml))
+	if err != nil {
+		return false, err
+	}
+	req.Header.Add("Content-Type", "text/xml; charset=utf-8")
+	req.Header.Add("SOAPAction", "http://tckimlik.nvi.gov.tr/WS/YabanciKimlikNoDogrula")
+	req.Header.Add("Host", "tckimlik.nvi.gov.tr")
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	response := ResponseForeign{}
 	if err := xml.NewDecoder(resp.Body).Decode(&response); err != nil {
 		return false, err
 	}
